1722e7eb77f4ffea357d331891f594c96343fcac
Houssemeddine SALEM <salemhoussemeddine@gmail.com>
2021-08-26 11:46:32 +0100

    final version

--- index.js
+++ index.js
@@ -1,35 +1,30 @@
 const express = require('express');
 const app = express();
 const port = '4000';
-const timer = require('./time')
+const time = require('./time');
+
 
 app.set('view engine', 'ejs');
 
 app.use(express.static(__dirname + "/public"));
 
+app.use((req, res, next) => {
+    let middlewareValue = time().areWeWorking;
+    (!middlewareValue) ? res.send('We are on a vacation') : next()
 
+})
 
-if (timer().currentDay !== 6 && timer().currentDay !== 7
-    && timer().currentHour < 20 && timer().currentHour > 9) {
-    app.get('/', (req, res) => {
-        console.log('this is a get request');
-        res.render('homepage')
-    });
+app.get('/', (req, res) => {
+    console.log('this is a get request');
+    res.render('homepage', { currentDate: time().dateFormated })
+});
 
-    app.get('/contactus', (req, res) => {
-        res.render('contact')
-    })
-
-    app.get('/ourservices', (req, res) => {
-        res.render('ourservices')
-    })
-} else {
-    app.get('/', (req, res) => {
-        console.log('this is a get request');
-        res.send('<p>we are on vacation</p>')
-    });
-}
+app.get('/:id', (req, res) => {
+    console.log(req.params)
+    res.render(req.params.id)
+})
 
 app.listen(port, () => {
     console.log('app is listening on port 4000');
 })
+
--- time.js
+++ time.js
@@ -4,11 +4,19 @@ const time = () => {
     const currentDay = today.getDay();
     const currentHour = today.getHours();
 
-    return {
+    let options = { weekday: 'long', year: 'numeric', month: 'long', day: 'numeric' };
+    let dateFormated = today.toLocaleDateString("en-US", options);
+    let areWeWorking = false;
 
-        currentDay,
-        currentHour
+    if (currentDay !== 6 && currentDay !== 7 && currentHour < 20 && currentHour > 9) {
+        areWeWorking = true;
     }
+    return {
+        areWeWorking,
+        dateFormated
+    };
+
+
 }
 
 module.exports = time;
